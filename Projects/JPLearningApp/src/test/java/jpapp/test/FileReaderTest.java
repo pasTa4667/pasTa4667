9e200de98418da4efab140922c0f83512a0796a3
Felix <felix.langbein@uni-ulm.de>
2022-06-16 16:26:24 +0200

    deleted chess updated jpapp

--- Projects/JPLearningApp/src/test/java/jpapp/test/FileReaderTest.java
+++ Projects/JPLearningApp/src/test/java/jpapp/test/FileReaderTest.java
@@ -5,6 +5,7 @@ import com.jp.jplearningapp.json.vocab.VocabReader;
 import com.jp.jplearningapp.json.kanji.KanjiReader;
 import com.jp.jplearningapp.json.kanji.KanjiWriter;
 import com.jp.jplearningapp.json.vocab.VocabWriter;
+import kotlin.text.Regex;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -28,33 +29,8 @@ public class FileReaderTest {
 
     @Test
     void toListTest(){
-        String[] paramList = new String[]{"cat","abc", "-", "abc", "<", "abc"};
-        String fin = null;
-        String fout = null;
-        boolean flag = false;
-        List<String> list = new ArrayList<>(Arrays.asList(paramList));
-        for(int i = 0; i < paramList.length; i++){
-            if(paramList[i].equals("<")){
-                fin = paramList[i +1];
-                if(!flag) list.remove(paramList[i + 1]);
-                list.remove("<");
-            }
-            if(paramList[i].equals(">")){
-                fout = paramList[i +1];
-                if(!flag) list.remove(paramList[i + 1]);
-                list.remove(">");
-            }
-            if(paramList[i].equals("-")){
-                flag = true;
-                list.remove("-");
-            }
-        }
-        String[] paramList01 = new String[list.size()];
-        for(int i = 0; i < paramList01.length; i++) paramList01[i] = list.get(i);
-
-        for(String s : paramList01){
-            System.out.print(s + ", ");
-        }
+        String str = "This website is for losers LOL!";
+        System.out.println(str.replaceAll("(a|e|i|o|u)", ""));
     }
 
 }
